9d47f1deb5e6c735120118305aace88a3e176e6d
evf <eliseuv@pm.me>
2026-02-02 19:55:42 -0300

    chore: config openssl lib link

--- flake.nix
+++ flake.nix
@@ -86,7 +86,13 @@
           # Compilation cache
           RUSTC_WRAPPER = "sccache";
           # OpenSSL config
-          # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (
+            with pkgs;
+            [
+              openssl
+            ]
+          );
 
           shellHook = ''
             cargo --version
